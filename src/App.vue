1192418c712b5f20125c58af3679828f33b354ea
Felix Wackernagel <felix.wackernagel@exedio.com>
2024-05-22 15:08:35 +0200

    add scss

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,15 @@ export default defineComponent({
 });
 </script>
 
-<style>
+<style lang="scss">
+$textColor: #2c3e50;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $textColor;
   margin-top: 60px;
 }
 </style>
