6994240208038751b0d26125090bf0c23f5d8f34
Felix Wackernagel <felix.wackernagel@exedio.com>
2024-05-23 16:12:05 +0200

    options api example

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <p>{{ name }} - {{ age }}</p>
+  <button @click="changeName('Zelda')">Change Name</button>
+  <button @click="changeAge(30)">Change Age</button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
-  components: {
-    HelloWorld,
+  components: {},
+  data() {
+    return {
+      name: "Link",
+      age: 25 as number | string,
+    };
+  },
+  methods: {
+    changeName(name: string) {
+      this.name = name;
+    },
+    changeAge(age: string | number) {
+      this.age = age;
+    },
   },
 });
 </script>
