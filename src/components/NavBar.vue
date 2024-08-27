826704e34429a2c16df101f84c18869f4b4b7a96
Felix Wackernagel <felix.wackernagel@gmail.com>
2024-08-27 23:56:22 +0200

    navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -18,7 +18,7 @@
         <RouterLink :to="{ name: 'about' }">{{ $t("navAbout") }}</RouterLink>
       </li>
     </ul>
-    <ul v-show="routeName.startsWith('droidFridge')">
+    <ul v-show="isDroidFridge">
       <li>
         <RouterLink :to="{ name: 'droidFridgeHome' }">{{
           $t("navDroidFridgeHome")
@@ -34,29 +34,33 @@
 </template>
 <script setup lang="ts">
 import { useRoute } from "vue-router";
+import { computed } from "vue";
 
 const route = useRoute();
-let routeName = route ? route.name : "";
-if (!routeName) {
-  routeName = "";
-}
+const isDroidFridge = computed(() => {
+  if (typeof route.name === "string") {
+    return (route.name as string).startsWith("droidFridge");
+  }
+  return false;
+});
 </script>
 <style lang="scss">
 nav {
   margin-bottom: 15px;
+  border: 1px solid var(--clr-primary);
+  border-radius: 8px;
+  background-color: var(--clr-primary);
 
   a {
     padding: 4px 8px;
     text-decoration: none;
     border-bottom: 2px solid transparent;
     transition: color 200ms ease, border-color 200ms ease;
-    color: var(--foreground);
+    color: var(--background);
 
     &:hover,
-    &.router-link--active,
     &.router-link--exact-active {
-      color: var(--clr-primary);
-      border-color: var(--clr-primary);
+      border-color: var(--background);
     }
   }
 
@@ -64,10 +68,17 @@ nav {
     display: flex;
     flex-direction: row;
     flex-wrap: nowrap;
+    padding: 8px;
 
     + ul {
-      margin-top: 16px;
+      border-top: 1px solid var(--background);
     }
+  }
+
+  li + li:before {
+    content: "/";
+    padding: 0 8px;
+    color: var(--background);
   }
 }
 </style>
